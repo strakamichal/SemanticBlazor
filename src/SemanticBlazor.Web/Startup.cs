1cac6154140f28d8978a8b766b6c2895f88df136
Michal Straka <info@strakamichal.com>
2020-11-14 08:56:41 +0100

    dev 1.4.0

--- src/SemanticBlazor.Web/Startup.cs
+++ src/SemanticBlazor.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SemanticBlazor.Web.Data;
+using Microsoft.Extensions;
 
 namespace SemanticBlazor.Web
 {
