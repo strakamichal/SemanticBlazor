37d6d0a8bdf921fcca449662978532ed71b17a49
Michal Straka <info@strakamichal.com>
2022-08-27 18:00:15 +0200

    refactoring

--- src/SemanticBlazor/JsFunc.cs
+++ src/SemanticBlazor/JsFunc.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.JSInterop;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using SemanticBlazor.Components.Base.Dropdown;
 
 namespace SemanticBlazor
 {
